38b31177c90f9059bc770564fd67e3d8f50e2235
Chijioke Nwachukwu <chijiokenwachukwu730@gmail.com>
2024-10-25 13:32:27 +0100

    update index.php
    i changed the index page layout

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-include "inc/controller.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -44,9 +41,43 @@ include "inc/controller.php";
     <!-- Preloader End -->
 
     <!-- Header Area Start -->
-    <?php 
-    include "inc/nav.php";
-    ?>
+    <header class="header-area header-sticky">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <nav class="main-nav">
+              <!-- Logo Start -->
+              <a
+                href="index.html"
+                class="navbar-brand d-flex align-items-center"
+              >
+                <img
+                  src="assets/images/logo.PNG"
+                  alt="WatchAnimeHQ"
+                  class="img-fluid w-auto"
+                style="max-height: 130px;"
+                />
+              </a>
+              <!-- Logo End -->
+
+              <!-- Menu Start -->
+              <ul class="nav">
+                <li><a href="index.html" class="active">Home</a></li>
+                <li><a href="anime-list.html">Anime List</a></li>
+                <li><a href="genres.html">Genres</a></li>
+                <!-- <li><a href="latest-episodes.html">Latest Episodes</a></li> -->
+                <li><a href="contact.html">Contact Us</a></li>
+                <li><a href="#">Sign In</a></li>
+              </ul>
+              <a class="menu-trigger">
+                <span>Menu</span>
+              </a>
+              <!-- Menu End -->
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
     <!-- Header Area End -->
 
     <!-- Main Banner Section Start -->
@@ -78,7 +109,7 @@ include "inc/controller.php";
           </div>
           <div class="col-lg-4 offset-lg-2">
             <div class="right-image">
-              <img src="assets/images/anime-banner.jpg" alt="Anime Banner" />
+              <img src="assets/images/single-game.jpg" alt="Anime Banner" />
             </div>
           </div>
         </div>
